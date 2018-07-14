5beae98b1ddd4966a30c9f832b5ae7fadd3d7fd4
PoojaPoluru <pooja.poluru95@gmail.com>
2018-07-14 09:41:53 -0700

    Initial Commit

---
+++ src/app/grocery-component/grocery-component.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grocery-component',
+  templateUrl: './grocery-component.component.html',
+  styleUrls: ['./grocery-component.component.css']
+})
+export class GroceryComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
